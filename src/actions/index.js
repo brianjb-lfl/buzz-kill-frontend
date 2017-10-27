f6c08c2de161539fb376f9834f7c3c8c15794d86
Brian Besaw <bjbesaw@outlook.com>
2017-10-27 10:33:20 -0400

    mod placeholders and labels on patron form and patron display, code cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,116 +3,115 @@ import 'whatwg-fetch';
  
 export const ADD_PATRON = 'ADD_PATRON';
 export const addPatron = (table, seat, gender, start) => dispatch => {
-    return fetch(`${REACT_APP_API_URL}/api/patrons/`, {
-        method: 'POST', 
-        body: JSON.stringify({
-            table, 
-            seat, 
-            gender, 
-            start
-        }), 
-        headers: {
+  return fetch(`${REACT_APP_API_URL}/api/patrons/`, {
+    method: 'POST', 
+    body: JSON.stringify({
+      table, 
+      seat, 
+      gender, 
+      start
+    }), 
+    headers: {
            'Content-Type': 'application/json' 
-        }
-    })
-    .then( res => {
-        if(!res.ok) {
-            return Promise.reject(res.statusText)
-        }
-        return res.json(); 
-    })
-    .then(patron => {
-        return dispatch(getPatrons()); 
-    })
-    .catch(err => {
-        return dispatch(handleError(err))
-    })
+    }
+  })
+  .then( res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText)
+    }
+    return res.json(); 
+  })
+  .then(patron => {
+    return dispatch(getPatrons()); 
+  })
+  .catch(err => {
+    return dispatch(handleError(err))
+  })
 }; 
 
 export const HANDLE_ERROR = 'HANDLE_ERROR'; 
 export const handleError = err => ({
-    type: HANDLE_ERROR, 
-    err
+  type: HANDLE_ERROR, 
+  err
 });
 
 export const CLEAR_ERROR = 'CLEAR_ERROR'; 
 export const clearError = () => ({
-    type: CLEAR_ERROR
+  type: CLEAR_ERROR
 });
 
 export const RENDER_PATRONS = 'RENDER_PATRONS'; 
 export const renderPatrons = patrons => ({
-    type: RENDER_PATRONS, 
-    patrons
+  type: RENDER_PATRONS, 
+  patrons
 }); 
 
 export const GET_PATRONS = 'GET_PATRONS'; 
 export const getPatrons = () => dispatch => {
-    return fetch(`${REACT_APP_API_URL}/api/patrons/`)
-        .then(res => {
-            if(!res.ok) {
-                return Promise.reject(res.statusText)
-            }
-            return res.json(); 
-        })
-        .then(data => {
-            dispatch(renderPatrons(data))
-            setTimeout(function(){dispatch(getPatrons())}, 60000); 
-        }
-    );
+  return fetch(`${REACT_APP_API_URL}/api/patrons/`)
+    .then(res => {
+      if(!res.ok) {
+        return Promise.reject(res.statusText)
+      }
+      return res.json(); 
+    })
+    .then(data => {
+      dispatch(renderPatrons(data))
+      setTimeout(function(){dispatch(getPatrons())}, 60000); 
+    });
 }
 
 export const ADD_DRINK = 'ADD_DRINK'; 
 export const addDrink = (quantity, patronId) => dispatch => { 
-    return fetch(`${REACT_APP_API_URL}/api/drinks/${patronId}`, {
-        method: 'PUT', 
-        body: JSON.stringify({
-            _id: patronId, 
-            drinks: {drinkEq: quantity}
-        }), 
-        headers: {
-           'Content-Type': 'application/json' 
-        }
-    })
-    .then(res => {
-        if(!res.ok) {
-            return Promise.reject(res.statusText)
-        }
-        return res.json(); 
-    })
-    .then(patron => {
-        dispatch(getPatrons()); 
-    });
+  return fetch(`${REACT_APP_API_URL}/api/drinks/${patronId}`, {
+    method: 'PUT', 
+    body: JSON.stringify({
+      _id: patronId, 
+      drinks: {drinkEq: quantity}
+    }), 
+    headers: {
+      'Content-Type': 'application/json' 
+    }
+  })
+  .then(res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText)
+    }
+    return res.json(); 
+  })
+  .then(patron => {
+    dispatch(getPatrons()); 
+  });
 }; 
 
 export const DELETE_PATRON = 'DELETE_PATRON'; 
 export const deletePatron = (patronId) => dispatch => {
-    return fetch(`${REACT_APP_API_URL}/api/patrons/${patronId}`, {
-        method: 'DELETE'
-    })
-    .then(res => {
-        if(!res.ok) {
-            return Promise.reject(res.statusText)
-        }
-        return;
-    })
-    .then(() => {
-        dispatch(getPatrons());
-    }); 
+  return fetch(`${REACT_APP_API_URL}/api/patrons/${patronId}`, {
+    method: 'DELETE'
+  })
+  .then(res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText)
+    }
+    return;
+  })
+  .then(() => {
+    dispatch(getPatrons());
+  }); 
 }
 
 export const DELETE_ALL = 'DELETE_ALL'; 
 export const deleteAll = () => dispatch => {
-    return fetch(`${REACT_APP_API_URL}/api/patrons/dayclose/`, {
-        method: 'DELETE'
-    })
-    .then(res => {
-        if(!res.ok) {
-            return Promise.reject(res.statusText)
-        }
-        return; 
-    })
-    .then(() => dispatch(getPatrons()))
+  return fetch(`${REACT_APP_API_URL}/api/patrons/dayclose/`, {
+    method: 'DELETE'
+  })
+  .then(res => {
+    if(!res.ok) {
+      return Promise.reject(res.statusText)
+    }
+    return; 
+  })
+  .then(() => dispatch(getPatrons()))
 }
 
 
--- src/components/Patron.js
+++ src/components/Patron.js
@@ -47,7 +47,7 @@ export class Patron extends React.Component {
         let callTaxi; 
         if(this.props.bac > 12.1) {
             callTaxi = (
-                <a href="tel:+15105934577">
+                <a href="tel:+18639448995">
                     <button className='button_red_patron'>Call Taxi <i className="fa fa-taxi" aria-hidden="true"></i></button>
                 </a>
             )
@@ -60,7 +60,7 @@ export class Patron extends React.Component {
                 </div>
                 <div className="right">
                     <i className="fa fa-times close" onClick={event => this.handleDeletePatron()} aria-hidden="true"></i>
-                    <h4>TIME SINCE START: {this.props.timeOnSite}</h4>
+                    <h4>VISIT CLOCK (HH:MM):  {this.props.timeOnSite}</h4>
                     <p>{this.props.seatString}</p>
                     {drinkDisplay}
                     <i className="fa fa-plus" onClick={event => this.handleFormSubmit(event)} aria-hidden="true"></i>
